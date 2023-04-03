bdeb59545d2ca86dca2e78f5266eb4c4e33c1849
ruchit.bhardwaj <ruchit@citta.cloud>
2023-04-03 10:56:03 -0700

    xgboost code improvement

--- src/alpha-xgboost-iris.py
+++ src/alpha-xgboost-iris.py
@@ -3,14 +3,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import xgboost as xgb
 from pyspark.sql import SparkSession
-from pyspark.sql import DataFrame
-import time
+from sklearn.preprocessing import LabelEncoder
 
+# initialize spark session
 spark = SparkSession.builder.appName('alpha-xgboost-iris').getOrCreate()
-print(spark)
-
-# appObjectTableProvider = spark._jvm.cloud.alpha.spark.providers.appobject.AppObjectTableProvider
-# print(appObjectTableProvider)
 
 iris_dataset = (spark.read
                 .format("cloud.alpha.spark.providers.appobject.AppObjectTableProvider")
@@ -18,49 +14,57 @@ iris_dataset = (spark.read
                 .option("rootDAGContextId", "2348")
                 .option("structTypeId", "4142")
                 .load())
-# print(f'iris_dataset: {iris_dataset.toPandas()}')
-iris_dataset_2 = iris_dataset.toPandas()
-# count1 = iris_dataset.limit(10).toJSON().collect()
-# print(f'count: {count1}')
-test = iris_dataset_2[["sepal_length_cm", "sepal_width_cm", "petal_length_cm", "petal_width_cm", "species"]]
-print(f'iris_dataset: {test.head()}')
 
-iris = load_iris()
-print(f'iris: {iris}')
-numSamples, numFeatures = iris.data.shape
-print(f'numSamples: {numSamples}, numFeatures: {numFeatures}')
-print(f'feature_names: {list(iris.feature_names)}')
-print(f'target_names: {list(iris.target_names)}')
-print(f'data: {iris.data}')
-print(f'target: {iris.target}')
+# converting spark dataframe to pandas dataframe
+iris_dataset = iris_dataset.toPandas()
 
-x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=42)
-# print(f'x_train: {x_train}')
-print(f'x_test: {x_test}')
-# print(f'y_train: {y_train}')
-print(f'y_test: {y_test}')
-#
-# train = xgb.DMatrix(x_train, label=y_train)
-# test = xgb.DMatrix(x_test, label=y_test)
-#
-# param = {
-#     'max_depth': 3,
-#     'eta': 0.3,
-#     'objective': 'multi:softmax',
-#     'num_class': 3
-# }
-#
-# epochs = 5
-#
-# # Train the model
-# model = xgb.train(param, train, epochs)
-#
-# # Predictions
-# predictions = model.predict(test)
-# print(f'Predictions: {predictions}')
-#
-# # Calculate accuracy
-# accuracy = accuracy_score(y_test, predictions)
-# print(f'Accuracy: {accuracy}')
-#
-# time.sleep(300)
+iris_dataset = iris_dataset[["sepal_length_cm", "sepal_width_cm", "petal_length_cm", "petal_width_cm", "species"]]
+
+# encode target labels with value starting from 0 and n_classes-1
+label_encoder = LabelEncoder()
+iris_dataset['species'] = label_encoder.fit_transform(iris_dataset['species'])
+
+# uncomment the following 2 lines in case we need to assign specific values to the classes
+# classification_column_mapping = {'Iris-setosa': 0, 'Iris-versicolor': 1, 'Iris-virginica': 2}
+# iris_dataset['encoded_species'] = iris_dataset['species'].map(classification_column_mapping)
+print(f'iris_dataset: \n{iris_dataset.head()}')
+
+# preparing data for training
+y = iris_dataset['species']
+# converting all columns to float (this is only essential if the input dataframe as some columns of type string)
+X = (iris_dataset.drop(['species'], axis=1)
+     .astype(float))
+
+# split data into train and test sets (20% for testing)
+x_train, x_test, y_train, y_test = train_test_split(X.values, y.values, test_size=0.2, random_state=42)
+
+# print shapes of the train and test sets
+print(f'x_train (shape): {x_train.shape}')
+print(f'y_train (shape): {y_train.shape}')
+print(f'x_test (shape): {x_test.shape}')
+print(f'y_test (shape): {y_test.shape}')
+
+# convert data into xgb format
+train = xgb.DMatrix(x_train, label=y_train)
+test = xgb.DMatrix(x_test, label=y_test)
+
+# set xgboost parameters
+param = {
+    'max_depth': 3,
+    'eta': 0.3,
+    'objective': 'multi:softmax',
+    'num_class': 3
+}
+
+epochs = 5
+
+# Train the model
+model = xgb.train(param, train, epochs)
+
+# Predictions
+predictions = model.predict(test).astype(int)
+print(f'Predictions: {predictions}')
+
+# Calculate accuracy
+accuracy = accuracy_score(y_test, predictions)
+print(f'Accuracy: {accuracy}')
