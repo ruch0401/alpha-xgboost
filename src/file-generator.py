b80d860933aca20b446a61cdf6dad8676b093df2
ruchit.bhardwaj <ruchit@citta.cloud>
2023-09-23 01:37:02 -0700

    Added file generation logic for all types of objects

--- src/file-generator.py
+++ src/file-generator.py
@@ -1,16 +1,19 @@
 import os
 import sys
+
 import pandas as pd
 
 # source_file_path = os.path.join("file://", desktop_path, "output.csv")
 source_file_path = "file:///home/ruchit/Desktop/output.csv"
-resources_root = '../resources'
+resources_root = '../output'
+
+# Suppress SettingWithCopyWarning
+pd.options.mode.chained_assignment = None  # 'None' means no warnings will be shown
 
 
 class CSVGeneratorParams:
-    def __init__(self, is_internal, is_external, object_name, total_files, records_per_file, inserts, updates):
-        self.is_internal = is_internal
-        self.is_external = is_external
+    def __init__(self, object_name, total_files, records_per_file, inserts, updates, part_of_candidate_key=None):
+        self.part_of_candidate_key = part_of_candidate_key
         self.object_name = object_name
         self.total_files = total_files
         self.records_per_file = records_per_file
@@ -25,7 +28,7 @@ def write_df_to_resources(final_df, path):
 
 
 def generate_csv(params: CSVGeneratorParams):
-    print('Generating CSV file with internal_id: ', internal_id, ' and external_id: ', external_id)
+    print('Starting CSV file generation')
 
     print('Reading source file')
     source_csv = pd.read_csv(source_file_path)
@@ -35,46 +38,89 @@ def generate_csv(params: CSVGeneratorParams):
     external_id_col_name = params.object_name + "_external_id"
     external_ids = [f'ext_{i}' for i in range(1, 10000000 + 1)]
     max_pool_size_required = params.total_files * params.records_per_file
+    source_csv[external_id_col_name] = external_ids
+    source_csv = source_csv.head(max_pool_size_required)
+    print(f'Selected {len(source_csv)} records from the source file')
+    handle_first_file_write(source_csv, external_ids, params)
+    for i in range(0, params.total_files):
+        start_index_for_inserts = params.inserts * (i + 1) + params.updates + 1
+        end_index_for_inserts = start_index_for_inserts + params.inserts - 1
 
-    if internal_id:
-        pass
+        start_index_for_updates = start_index_for_inserts - params.inserts
+        end_index_for_updates = start_index_for_updates + params.updates - 1
 
-    if external_id:
-        source_csv[external_id_col_name] = [f'ext_{i}' for i in range(1, 10000000 + 1)]
-        source_csv = source_csv.head(max_pool_size_required)
-        print(f'Selected {len(source_csv)} records from the source file')
-        handle_first_file_write(source_csv, external_id_col_name, external_ids, params)
-        for i in range(2, params.total_files + 2):
-            start_index_for_inserts = params.inserts * (i - 1) + params.updates + 1
-            end_index_for_inserts = start_index_for_inserts + params.inserts - 1
+        print(f'Inserts: ({start_index_for_inserts}, {end_index_for_inserts}) for iteration {i + 1}')
+        print(f'Updates: ({start_index_for_updates}, {end_index_for_updates}) for iteration {i + 1}')
 
-            start_index_for_updates = start_index_for_inserts - params.inserts
-            end_index_for_updates = start_index_for_updates + params.updates - 1
+        insert_ids = [f'ext_{i}' for i in range(start_index_for_inserts, end_index_for_inserts + 1)]
+        update_ids = [f'ext_{i}' for i in range(start_index_for_updates, end_index_for_updates + 1)]
+        required_rows_inserts = source_csv[source_csv[external_id_col_name].isin(insert_ids)]
+        required_rows_updates = source_csv[source_csv[external_id_col_name].isin(update_ids)]
 
-            print(f'Inserts: ({start_index_for_inserts}, {end_index_for_inserts}) for iteration {i - 1}')
-            print(f'Updates: ({start_index_for_updates}, {end_index_for_updates}) for iteration {i - 1}')
+        if is_internal_id_required():
+            required_rows_inserts.rename(columns={external_id_col_name: internal_id_col_name}, inplace=True)
+            required_rows_inserts[internal_id_col_name] = required_rows_inserts[internal_id_col_name].apply(
+                lambda x: x.replace(x, ''))
+            required_rows_updates.rename(columns={external_id_col_name: internal_id_col_name}, inplace=True)
+            required_rows_updates[internal_id_col_name] = required_rows_updates[internal_id_col_name].str.split('_').str[1]
 
-            insert_ids = [f'ext_{i}' for i in range(start_index_for_inserts, end_index_for_inserts + 1)]
-            update_ids = [f'ext_{i}' for i in range(start_index_for_updates, end_index_for_updates + 1)]
-            required_rows_inserts = source_csv[source_csv[external_id_col_name].isin(insert_ids)]
-            required_rows_updates = source_csv[source_csv[external_id_col_name].isin(update_ids)]
-            final_df = pd.concat([required_rows_inserts, required_rows_updates], axis=0)
-            write_df_to_resources(final_df, f'{resources_root}/{params.object_name}_{i - 1}.csv')
+        if is_external_id_required():
+            print('Default code path which handles the external_id case is being executed')
+
+        if is_conditional_key_required():
+            required_rows_inserts = required_rows_inserts.remove(columns=[external_id_col_name])
+            required_rows_updates = required_rows_updates.remove(columns=[external_id_col_name])
+            # all the candidate keys will have to be changed to simulate inserts
+            candidate_keys = params.part_of_candidate_key.split(',')
+            for key in candidate_keys:
+                required_rows_inserts[key] = required_rows_inserts[key].apply(lambda x: x + str(i + 1))
+
+            # one of the other columns, lets say the 'amount' column will have to be changed to simulate updates, keeping the candidate keys same
+            required_rows_updates['amount'] = required_rows_updates['amount'].apply(lambda x: int(x) + 1)
+
+        final_df = pd.concat([required_rows_inserts, required_rows_updates], axis=0)
+        write_df_to_resources(final_df, f'{resources_root}/{params.object_name}_{i + 1}.csv')
 
 
-def handle_first_file_write(df, external_id_col_name, external_ids, params):
-    # first file is a special case where we need to insert all the records i.e. inserts + updates
-    print('Handling first file write')
+def is_internal_id_required():
+    return 'is_standard' in sys.argv
+
+
+def is_external_id_required():
+    return ('is_standard_with_external_id' in sys.argv
+            or 'is_standard_bitemporal' in sys.argv
+            or 'is_versioned_bitemporal' in sys.argv)
+
+
+def is_conditional_key_required():
+    return ('is_standard_transactional' in sys.argv
+            or 'is_versioned_transactional' in sys.argv
+            or 'is_complete_snapshot' in sys.argv
+            or 'is_incremental_snapshot' in sys.argv)
+
+
+def handle_first_file_write(df, external_ids, params):
+    internal_id_col_name = params.object_name + "_id"
+    external_id_col_name = params.object_name + "_external_id"
     ids = external_ids[:(params.inserts + params.updates)]
-    df = df[df[external_id_col_name].isin(ids)]
-    print(f'Selected {len(df)} records for handling first file write')
+    df = df[df[external_id_col_name].isin(
+        ids)]  # default case is all object types which require external_id (standard_with_external_id, standard_bitemporal, versioned_bitemporal)
+
+    if is_internal_id_required():
+        df.rename(columns={external_id_col_name: internal_id_col_name}, inplace=True)
+        df[internal_id_col_name] = df[internal_id_col_name].apply(lambda x: x.replace(x, ''))  # since internal_id is auto-generated, we need to remove the values from the first file
+
+    if is_external_id_required():
+        print('Default code path which handles the external_id case is being executed')
+
+    if is_conditional_key_required():
+        df = df.remove(columns=[external_id_col_name])
+
     write_df_to_resources(df, f'{resources_root}/{params.object_name}_base.csv')
 
 
-if __name__ == '__main__':
-    print('System arguments passed to the code are: ', sys.argv)
-
-    global object_name, records_per_file, total_files, inserts, updates
+def parse_command_line():
+    global object_name, records_per_file, total_files, inserts, updates, part_of_candidate_key
     if 'object_name' not in sys.argv:
         print('Object name is required in the system arguments')
         sys.exit(1)
@@ -105,18 +151,26 @@ if __name__ == '__main__':
     else:
         updates = int(sys.argv[sys.argv.index('updates') + 1])
 
-    global internal_id, external_id
-    if 'is_standard_object' in sys.argv:
-        internal_id = True
-        external_id = False
-        generate_csv(
-            CSVGeneratorParams(internal_id, external_id, object_name, total_files, records_per_file, inserts, updates))
+    if is_conditional_key_required():
+        if 'part_of_candidate_key' not in sys.argv:
+            print('part_of_candidate_key is required in the system arguments when the object type is set to transactional or snapshot')
+            sys.exit(1)
+        part_of_candidate_key = sys.argv[sys.argv.index('part_of_candidate_key') + 1]
 
-    if 'is_standard_with_external_id' in sys.argv:
-        internal_id = False
-        external_id = True
-        generate_csv(
-            CSVGeneratorParams(internal_id, external_id, object_name, total_files, records_per_file, inserts, updates))
+
+if __name__ == '__main__':
+    print('System arguments passed to the code are: ', sys.argv)
+
+    object_name = None
+    total_files = None
+    records_per_file = None
+    inserts = None
+    updates = None
+    part_of_candidate_key = None
+
+    parse_command_line()
+    generate_csv(
+        CSVGeneratorParams(object_name, total_files, records_per_file, inserts, updates, part_of_candidate_key))
 
     # total_files = 4
     # inserts = 70
